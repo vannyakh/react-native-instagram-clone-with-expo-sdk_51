0b42288f00d3a07593f3e970c2e9b9fe13791f4a
VANN YA <vannakkm33@gmail.com>
2024-08-08 11:33:59 +0700

    feat: Add new tab components for Add, Reels, Search, and Account
    This commit adds new tab components for Add, Reels, Search, and Account. Each component includes a basic structure with a View and Text element. The components are imported and exported from their respective files. Stylesheets are also included, although they are currently empty.
    
    Co-authored-by: [Author Name]

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -22,35 +22,41 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          headerShown:false,
+          title: 'Home',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="search"
         options={{
-          title: 'Tab Two',
+          title: 'Search',
+          tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="add"
+        options={{
+          title: 'Add',
+          tabBarIcon: ({ color }) => <TabBarIcon name="plus" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="reels"
+        options={{
+          title: 'Reels',
+          tabBarIcon: ({ color }) => <TabBarIcon name="film" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Account',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,129 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-
-export default function TabOneScreen() {
+import { Image, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, useThemeColor, View } from "@/components/Themed";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
+export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* header */}
+      <View style={styles.headerTop}>
+        {/* logo left */}
+        <Image
+          source={require("@/assets/images/icons/ig_logo.png")}
+          style={{ width: 120, height: 34, resizeMode: "contain" }}
+        />
+        {/* action right */}
+        <View style={styles.actioneRight}>
+          <TouchableOpacity>
+            <Image
+              source={require("@/assets/images/icons/love.png")}
+              style={{ width: 26, height: 26, resizeMode: "contain" }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image
+              source={require("@/assets/images/icons/chat.png")}
+              style={{ width: 26, height: 26, resizeMode: "contain" }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      {/* body */}
+      <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+        {/* story */}
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{ paddingVertical: 10, paddingHorizontal: 16 }}
+        >
+          <TouchableOpacity
+            style={{ alignItems: "center", marginHorizontal: 4 }}
+          >
+            <View style={[styles.myStory,{borderColor: "#01D5FF",}]}>
+              <Image
+                source={require("@/assets/images/avatars/avata-1.jpg")}
+                style={{ width: "100%", height: "100%", borderRadius: 30 }}
+              />
+            </View>
+            <Text style={[styles.storyLabel, { color: "#999" }]}>story</Text>
+          </TouchableOpacity>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item,index) => (
+             <TouchableOpacity
+             style={{ alignItems: "center", marginHorizontal: 4 }}
+             key={index}
+           >
+             <LinearGradient colors={["#FF5B5B", "#FFC530"]}
+               style={{
+                 padding: 1.5,
+                 borderRadius: 30,
+               }}
+             > 
+               <View style={[styles.story,{borderColor: "#fff",}]}>
+                 <Image
+                   source={require("@/assets/images/avatars/avata-1.jpg")}
+                   style={{ width: "100%", height: "100%", borderRadius: 30 }}
+                 />
+               </View>
+             </LinearGradient>
+             <Text
+               style={[styles.storyLabel, { color: "#999" }]}
+             >user{index}</Text>
+           </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+  },
+  headerTop: {
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 10,
+  },
+  actioneRight: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  storyLabel: {
+    fontSize: 12,
+    fontWeight: "regular",
+    marginVertical: 10,
+  },
+  myStory: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+  },
+  story: {
+    width: 58,
+    height: 58,
+    borderRadius: 30,
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+    
   },
 });
